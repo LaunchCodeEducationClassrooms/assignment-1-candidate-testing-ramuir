1466a02215ed91c2e56e2f654acdbedfde834c99
Ryan A Muir <ryanadrianmuir@gmail.com>
2021-02-19 06:05:23 +0000

    initial commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,22 +5,28 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = '';
+let questions = ["Who was the first American woman in space? ","True or False: 5000 meters = 5 kilometers. ","(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the International Space Station (ISS)? "] ;
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"] ;
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  for (let i=0;i<questions.length;i++){
+    let answer = input.question(`${i+1})${questions[i]}`);
+    candidateAnswers.push(answer);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}`);
+    console.log("");
+  }
 
 }
 
@@ -28,9 +34,26 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-  let grade;
+  // if (candidateAnswer === correctAnswer){
+  //   console.log("That is correct!");
+  // } 
+  // else if (candidateAnswer.toLowerCase() == correctAnswer.toLowerCase()){
+  //   console.log("That is correct!");
+  // }
+  // else{
+  //   console.log("That is not correct!");
+  // }
   
+   let intersection = correctAnswers.filter(x => candidateAnswers.includes(x));
+   
+
+  let grade = (intersection.length / correctAnswers.length) * 100;
+  console.log(`>>> Overall Grade: ${grade}% (${intersection.length} of 5 responses correct) <<<`);
+  if(intersection.length<4){
+    console.log(">>>Status: FAILED <<<");
+  }else{
+    console.log(">>> Status: PASS <<<");
+  }
 
   return grade;
 }
@@ -38,7 +61,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Candidate Name: ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
